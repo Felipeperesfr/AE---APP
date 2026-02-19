ca1c77e091f02f5e5d24c198c297878ccfb2b830
Felipeperesfr <85023062+Felipeperesfr@users.noreply.github.com>
2026-02-19 09:33:27 -0300

    blueprint

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -571,6 +571,8 @@ cron.schedule(
   }
 );
 
-app.listen(3001, () => {
-  console.log("Local Host inicializado");
-});
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+}); 
